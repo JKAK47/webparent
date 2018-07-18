8630b4b842272acd2fb942168c296a4f4308cbf5
PengRong <1557862201@qq.com>
2018-07-18 11:59:24 +0800

    Initial commit

---
+++ web/src/main/java/org/vincent/web/App.java
@@ -0,0 +1,6 @@
+package org.vincent.web;
+/**
+ * Created by PengRong on 2018/7/18.
+ */
+public class App {
+}
---
+++ webfacade/src/main/java/org/vincent/web/facade/TypeConstants.java
@@ -0,0 +1,7 @@
+package org.vincent.web.facade;
+
+/**
+ * Created by PengRong on 2018/7/18.
+ */
+public class TypeConstants {
+}
