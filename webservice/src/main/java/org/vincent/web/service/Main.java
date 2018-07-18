d93f3d217c30e1c3bc3925292e2e3b8ee99a6d8d
PengRong <1557862201@qq.com>
2018-07-18 15:48:52 +0800

    对于三个 组件 web, webfacade,webservice 的插件进行配置。

---
+++ webservice/src/main/java/org/vincent/web/service/Main.java
@@ -0,0 +1,12 @@
+package org.vincent.web.service;
+
+/**
+ * Service 入口Main 类
+ * Created by PengRong on 2018/7/18.
+ */
+public class Main {
+    public static void main( String[] args ){
+        System.out.println("begin start web-service...");
+        System.out.println("begin stop web-service...");
+    }
+}
