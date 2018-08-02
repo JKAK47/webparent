a02453f39b3c0dede171b9b9885a780d6f383ccb
PengRong <1557862201@qq.com>
2018-08-02 14:39:06 +0800

    修改  ${project.build.version} 属性获取不到的问题

--- web/src/main/java/org/vincent/web/front/App.java
+++ web/src/main/java/org/vincent/web/front/App.java
@@ -3,4 +3,7 @@ package org.vincent.web.front;
  * Created by PengRong on 2018/7/18.
  */
 public class App {
+    public  static  void  main(String[] args){
+        System.out.println("sdfs");
+    }
 }
