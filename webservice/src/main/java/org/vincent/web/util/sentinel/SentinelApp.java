5dcdc4b9eb20d13b40b99c4cc170dc4eb61428d2
PengRong <1557862201@qq.com>
2018-08-07 16:37:46 +0800

    去除dao,facade组件 finalName属性配置

---
+++ webservice/src/main/java/org/vincent/web/util/sentinel/SentinelApp.java
@@ -0,0 +1,46 @@
+package org.vincent.web.util.sentinel;
+
+import com.alibaba.csp.sentinel.Entry;
+import com.alibaba.csp.sentinel.SphU;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.alibaba.csp.sentinel.slots.block.RuleConstant;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * https://github.com/alibaba/Sentinel/wiki/%E4%BB%8B%E7%BB%8D?spm=5176.214990.1146036.2.13bc187fmHea4V
+ * Created by PengRong on 2018/8/7.
+ */
+public class SentinelApp
+{
+    public  static void  main(String[] args){
+        initFlowRules();
+        while (true) {
+            Entry entry = null;
+            try {
+                entry = SphU.entry("HelloWorld");
+                System.out.println("hello world");
+            } catch (BlockException e1) {
+                System.out.println("block!");
+            } finally {
+                if (entry != null) {
+                    entry.exit();
+                }
+            }
+        }
+    }
+
+    private static void initFlowRules() {
+        List<FlowRule> rules = new ArrayList<FlowRule>();
+        FlowRule rule = new FlowRule();
+        rule.setResource("HelloWorld");
+        rule.setGrade(RuleConstant.FLOW_GRADE_QPS);
+        // Set limit QPS to 20.
+        rule.setCount(20);
+        rules.add(rule);
+        FlowRuleManager.loadRules(rules);
+    }
+}
