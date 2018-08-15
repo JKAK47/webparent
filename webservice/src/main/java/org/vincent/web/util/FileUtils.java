46b08592cfc573d9798a996e0e105a9982a969e3
PR <1557862201@qq.com>
2018-08-16 02:01:58 +0800

    增加一个根据file实例将所有确实的文件夹和文件创建的工具方法。

--- webservice/src/main/java/org/vincent/web/util/FileUtils.java
+++ webservice/src/main/java/org/vincent/web/util/FileUtils.java
@@ -7,72 +7,112 @@ import java.util.List;
 
 /**
  * Created by PengRong on 2018/8/8.
+ *
  * @author PengRong
  * @date 2018/08/08
  */
 public class FileUtils {
-    public  static  void  main(String[] args) throws IOException {
-        List<String> list=new ArrayList<>();
-        getFileNameList("D:\\DowJones\\HK\\20170906\\",list);
-        System.out.println(list);
-        clear(null);
+		/**
+		 * 文件名组成：文件名 + 扩展名 ，这两部分间通过点号区分
+		 */
+		public static final String DOT = ".";
 
-    }
-    /**
-     * 清除 file 实例所指向的文件内容 </p>
-     * @param file 待清空内容的文件 </p>
-     */
-    public static void clear(File file) throws IOException {
-        if (file ==null ) {
-            return;
-        }
-        if (file.exists() && file.isFile()){
-            //删除文件
-            boolean isDelete=file.delete();
-            if (isDelete){
-                //新建文件
-                file.createNewFile();
-            }
-        }
-    }
+		public static void main(String[] args) throws IOException {
+				List<String> list = new ArrayList<>();
+				getFileNameList("D:\\DowJones\\HK\\20170906\\", list);
+				System.out.println(list);
+				clear(null);
 
-    /**
-     *  根据文件夹路径，递归调用获取该文件夹下所有文件 </p>
-     * @param filepath 待遍历的文件夹目录 </p>
-     * @param filePathList 所有文件的绝对路径 List
-     * @return 返回遍历目录下所有文件的绝对路径List
-     */
-    public static List<String> getFileNameList(String filepath, List<String> filePathList) throws IOException {
+		}
 
-        if (filepath == null){
-            return filePathList;
-        }
-        if(null == filePathList){
-            filePathList = new ArrayList<>();
-        }
-        /** 去除 filepath目录 字符串最后的路径分隔符 */
-        if (filepath.substring(filepath.length()-1).equalsIgnoreCase(File.separator) ){
-            filepath=filepath.substring(0,filepath.length()-1);
-        }
-        File file = new File(filepath);
-        if (!file.isDirectory()) {
-            filePathList.add(file.getCanonicalFile().getPath());
-        } else if (file.isDirectory()) {
-            /** 获取当前目录下所有文件的文件名: 包括文件和文件夹； 单纯只是文件名和文件夹名 */
-            String[] fileList = file.list();
-            for (int i = 0; i < fileList.length; i++) {
-                File readfile = new File(filepath + File.separator + fileList[i]);
-                //过滤压缩文件
+		/**
+		 * 清除 file 实例所指向的文件内容 </p>
+		 *
+		 * @param file 待清空内容的文件 </p>
+		 */
+		public static void clear(File file) throws IOException {
+				if (file == null) {
+						return;
+				}
+				if (file.exists() && file.isFile()) {
+						//删除文件
+						boolean isDelete = file.delete();
+						if (isDelete) {
+								//新建文件
+								file.createNewFile();
+						}
+				}
+		}
+
+		/**
+		 * 根据一个file 实例，代表一个抽象的目录字符串，抽象路径字符串必须是绝对路径。
+		 * 这个字符串可能代表一个目录，可能代表一个文件的路径字符串，其中的目录，最后的文件不存在，这个方法是把不存在的目录和文件创建成功。
+		 *
+		 * @param file 虚拟字符串创建的file 实例
+		 */
+		public static File touchFile(File file) throws IOException {
+				if (!file.exists()) {
+						/** 去除 filepath目录 字符串最后的路径分隔符 */
+						String filepath = file.getCanonicalPath();
+						if (filepath.substring(filepath.length() - 1).equalsIgnoreCase(File.separator)) {
+								filepath = filepath.substring(0, filepath.length() - 1);
+						}
+						file=new File(filepath);
+						//判定当前file表示的抽象字符串是否是文件还是文件夹，根据目录最后的文件或文件夹名称是否有点
+						String name = file.getName();
+						//是文件
+						if (name != null && name.indexOf(FileUtils.DOT) != -1) {
+								if (!file.getParentFile().exists()) {
+										file.getParentFile().mkdirs();
+								}
+								file.createNewFile();
+						} else {
+								// 是目录
+								file.mkdirs();
+						}
+
+				}
+				return file;
+		}
+
+		/**
+		 * 根据文件夹路径，递归调用获取该文件夹下所有文件路径字符串 </p>
+		 *
+		 * @param filepath     待遍历的文件夹目录 </p>
+		 * @param filePathList 所有文件的绝对路径 List
+		 * @return 返回遍历目录下所有文件的绝对路径List
+		 */
+		public static List<String> getFileNameList(String filepath, List<String> filePathList) throws IOException {
+
+				if (filepath == null) {
+						return filePathList;
+				}
+				if (null == filePathList) {
+						filePathList = new ArrayList<>();
+				}
+				/** 去除 filepath目录 字符串最后的路径分隔符 */
+				if (filepath.substring(filepath.length() - 1).equalsIgnoreCase(File.separator)) {
+						filepath = filepath.substring(0, filepath.length() - 1);
+				}
+				File file = new File(filepath);
+				if (!file.isDirectory()) {
+						filePathList.add(file.getCanonicalFile().getPath());
+				} else if (file.isDirectory()) {
+						/** 获取当前目录下所有文件的文件名: 包括文件和文件夹； 单纯只是文件名和文件夹名 */
+						String[] fileList = file.list();
+						for (int i = 0; i < fileList.length; i++) {
+								File readfile = new File(filepath + File.separator + fileList[i]);
+								//过滤压缩文件
                 /*if (readfile.getName().endsWith(".zip")) {
                     continue;
                 }*/
-                if (!readfile.isDirectory()) {
-                    filePathList.add(readfile.getPath());
-                } else if (readfile.isDirectory()) {
-                    getFileNameList(filepath + File.separator + fileList[i],filePathList);
-                }
-            }
-        }
-        return filePathList;
-    }
+								if (!readfile.isDirectory()) {
+										filePathList.add(readfile.getPath());
+								} else if (readfile.isDirectory()) {
+										getFileNameList(filepath + File.separator + fileList[i], filePathList);
+								}
+						}
+				}
+				return filePathList;
+		}
 }
--- webservice/src/test/java/org/vincent/web/util/xml/JAXBUtilsTest.java
+++ webservice/src/test/java/org/vincent/web/util/xml/JAXBUtilsTest.java
@@ -1,71 +1,88 @@
 package org.vincent.web.util.xml;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import javax.xml.bind.JAXBException;
 import org.junit.Assert;
 import org.junit.Test;
 import org.vincent.web.service.bean.Customer;
-
-import javax.xml.bind.JAXBException;
-import java.io.File;
-import java.net.URL;
-import java.util.regex.Matcher;
+import org.vincent.web.util.FileUtils;
 
 
 /**
  * Unit test for JAXBUtilsTest App.
  */
-public class JAXBUtilsTest
-{
-    /**
-     * 从XML文件里面读取数据并转成JavaBean 数据。
-     *
-     */
-    @Test
-    public void testUnmarshaller() throws JAXBException {
-        URL url =ClassLoader.getSystemClassLoader().getResource("test/customer.xml");
-        if (!(new File(url.getFile()).exists())){
-            Assert.fail("文件不存在");
-        }
-        Customer customer=JAXBUtils.Unmarshaller(Customer.class,url.getFile());
-        System.out.println(customer);
+public class JAXBUtilsTest {
+		/**
+		 * 从XML文件里面读取数据并转成JavaBean 数据。
+		 */
+		@Test
+		public void testUnmarshaller() throws JAXBException {
+				URL url = ClassLoader.getSystemClassLoader().getResource("test/customer.xml");
+				if (!(new File(url.getFile()).exists())) {
+						Assert.fail("文件不存在");
+				}
+				Customer customer = JAXBUtils.Unmarshaller(Customer.class, url.getFile());
+				System.out.println(customer);
 
-    }
+		}
 
-    @Test
-    public  void  testMarshaller() throws JAXBException {
-        Customer customer=new Customer();
-        customer.setCustomerAge(35);
-        customer.setId("124569");
-        customer.setName("GAGA");
-        /** test类的  /D:/Dev/WorkStation/IdeaProjects/webparent/webservice/target/test-classes/*/
-        System.out.println(ClassLoader.getSystemClassLoader().getResource("").getFile());
-        URL url =ClassLoader.getSystemClassLoader().getResource("test/customer-outpupt.xml");
-        System.out.println(url.getFile());
-        JAXBUtils.Marshaller(customer,url.getFile());
-        String[] str="19630000$19640000".split(Matcher.quoteReplacement("$"));
-        for (String A: str
-             ) {
-            System.out.println(A);
-        }
-        /**
-         * 返回的结果是 \\\s
-         * */
-        System.out.println(Matcher.quoteReplacement("\\$"));
-        /**
-         * 返回的是 asd-?
-         *
-         * */
-        System.out.println(Matcher.quoteReplacement("asd-?"));
-        /**
-         * Matcher.quoteReplacement 方法是 如果 入参如果没有 \,$ 两个字符，就原样返回；
-         * 如果有这两个字符，对这两个字符的前面添加转义字符 \
-         * 变成 \\,\$ 两种形式。
-         */
+		/**
+		 * 将JavaBean 数据转换为xml 数据并存放在xml 文件里面
+		 *
+		 * @throws JAXBException
+		 */
+		@Test
+		public void testMarshaller() throws JAXBException, IOException {
+				Customer customer = new Customer();
+				customer.setCustomerAge(35);
+				customer.setId("124569");
+				customer.setName("GAGA");
+				/**
+				 * test类的  /D:/Dev/WorkStation/IdeaProjects/webparent/webservice/target/test-classes/
+				 */
+				String baseDir = ClassLoader.getSystemClassLoader().getResource("").getFile();
+				System.out.println(baseDir);
+				/** file 就是一个文件 */
+				File file = new File(baseDir, "test/customer-outpupt.xml");
+				if (file.exists()) {
+						file.delete();
+				}
+				System.out.println(Arrays.asList(new File(baseDir).list()));
+				/**
+				 * 给定File 实例，不管是文件夹字符串还是文件字符串，我们需要将不存在的目录和文件创建
+				 */
+				FileUtils.touchFile(file);
+				URL url = ClassLoader.getSystemClassLoader().getResource("test/customer-outpupt.xml");
+				System.out.println(url.getFile());
+				JAXBUtils.Marshaller(customer, url.getFile());
+				String[] str = "19630000$19640000".split(Matcher.quoteReplacement("$"));
+				for (String A : str) {
+						System.out.println(A);
+				}
+				/**
+				 * 返回的结果是 \\\s
+				 * */
+				System.out.println(Matcher.quoteReplacement("\\$"));
+				/**
+				 * 返回的是 asd-?
+				 *
+				 * */
+				System.out.println(Matcher.quoteReplacement("asd-?"));
+				/**
+				 * Matcher.quoteReplacement 方法是 如果 入参如果没有 \,$ 两个字符，就原样返回；
+				 * 如果有这两个字符，对这两个字符的前面添加转义字符 \
+				 * 变成 \\,\$ 两种形式。
+				 */
 
-        str="oboo:and:fooy".split("o");
+				str = "oboo:and:fooy".split("o");
 
-        System.out.println("$".indexOf('\\') );
-        System.out.println("$".indexOf('$') );
-    }
+				System.out.println("$".indexOf('\\'));
+				System.out.println("$".indexOf('$'));
+		}
 
 
 }
